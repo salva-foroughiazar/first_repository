e7315e6254da8b851e99503e08a2a4eb218be2ce
salva foroughi azar <salva.fr91@gmail.com>
2023-09-08 23:12:08 +0330

    Add homework file to js directory

---
+++ js/homework.js
@@ -0,0 +1,21 @@
+console.log(10 + 10);
+console.log(15 - 10);
+console.log(10 + "Hi");
+console.log(10 ** 2);
+console.log(10 / 2);
+console.log(10 % 2);
+console.log("hi" + "dear");
+console.log(10 > 15);
+console.log(10 <= 10);
+console.log(10 === "10");
+console.log(10 == 10);
+
+// part 2
+const name = "Salva";
+const Lname = "Foroughi"
+console.log(name + Lname);
+
+let i = 0;
+for (i = 0; i<10 ; i++) {
+    console.log(i);
+}
