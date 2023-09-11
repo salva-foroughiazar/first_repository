19a122cde93f2c97cd86050b18f496dd9bd73c04
salva foroughi azar <salva.fr91@gmail.com>
2023-09-12 00:26:41 +0330

    Days practice

---
+++ session3/p4.ts
@@ -0,0 +1,47 @@
+// switch-case
+const day : number = 8;
+switch (day) {
+    case 1:
+        console.log("Shanbe");
+        break;
+    case 2:
+        console.log("YekShanbe");
+        break;
+    case 3:
+        console.log("DoShanbe");
+        break;
+    case 4:
+        console.log("SeShanbe");
+        break;
+    case 5:
+        console.log("CharShanbe");
+        break;
+    case 6:
+        console.log("PanjShanbe");
+        break;
+    case 7:
+        console.log("Jome");
+        break;
+    default :
+        console.log("eshtebah mizani dadash :D");
+        break;
+}
+
+// if-else
+if( day === 1) {
+    console.log("Shanbe");
+} else if ( day === 2){
+    console.log("1Shanbe");
+} else if ( day === 3){
+    console.log("2Shanbe");
+} else if ( day === 4){
+    console.log("3Shanbe");
+} else if ( day === 5){
+    console.log("4Shanbe");
+} else if ( day === 6){
+    console.log("5Shanbe");
+} else if ( day === 7){
+    console.log("Jomee");
+} else {
+    console.log("eshtebah mizani dadash :D")
+}
