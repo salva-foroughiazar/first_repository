0b13a36db11b429838ab1622a38870753309f8a1
salva foroughi azar <salva.fr91@gmail.com>
2023-09-17 16:29:28 +0330

    Done4

--- session4/p3.ts
+++ session4/p3.ts
@@ -14,3 +14,12 @@ AddToContact("salva" , 123);
 AddToContact("mojtaba" , 456);
 console.log(ContactList);
 
+function search(Sname:string) {
+    let SearchResult = ContactList.find(element => element.name === Sname);
+    if (SearchResult){
+        console.log("Done!")
+    } else {
+        console.log("Not here!")
+    }
+}
+search("salva");
